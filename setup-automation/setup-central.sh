7a295b8b5a09f4b298d9fd31b50f2272f5ef478a
Nuno Martins <nmartins@redhat.com>
2026-03-10 12:52:28 +0200

    Update setup-central.sh

--- setup-automation/setup-central.sh
+++ setup-automation/setup-central.sh
@@ -6,10 +6,10 @@ systemctl disable systemd-tmpfiles-setup.service
 nmcli connection add type ethernet con-name eth1 ifname eth1 ipv4.addresses 192.168.1.11/24 ipv4.method manual connection.autoconnect yes
 nmcli connection up eth1
 echo "192.168.1.10 control.lab control" >> /etc/hosts
-echo "192.168.1.11 central.lab control" >> /etc/hosts
-echo "192.168.1.12 vault.lab control" >> /etc/hosts
-echo "192.168.1.13 wazuh.lab control" >> /etc/hosts
-echo "192.168.1.14 node01.lab control" >> /etc/hosts
+echo "192.168.1.11 central.lab central" >> /etc/hosts
+echo "192.168.1.12 vault.lab vault" >> /etc/hosts
+echo "192.168.1.13 wazuh.lab wazuh" >> /etc/hosts
+echo "192.168.1.14 node01.lab node01" >> /etc/hosts
 
 
 rm -rf /etc/yum.repos.d/*
