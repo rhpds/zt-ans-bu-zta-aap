1f8ddac3c6a165c6902e9727745ac180ca74d85a
Alberto Gonzalez Rodriguez <josegonz@redhat.com>
2026-03-13 10:24:45 +0100

    Update setup-central.sh

--- setup-automation/setup-central.sh
+++ setup-automation/setup-central.sh
@@ -163,3 +163,18 @@ EOF
 
 ansible-playbook -i /tmp/inventory /tmp/zta-setup.yml
 
+tee /etc/httpd/conf.d/ipa-rewrite.conf << IPA
+# VERSION 7 - DO NOT REMOVE THIS LINE
+
+RequestHeader set Host idmprimary.example.local
+RequestHeader set Referer https://idmprimary.example.local/ipa/ui/
+RewriteEngine on
+
+# Rewrite for plugin index, make it like it's a static file
+RewriteRule ^/ipa/ui/js/freeipa/plugins.js$    /ipa/wsgi/plugins.py [PT]
+
+RewriteCond %{HTTP_HOST}    ^ipa-ca.example.local$ [NC]
+RewriteCond %{REQUEST_URI}  !^/ipa/crl
+RewriteCond %{REQUEST_URI}  !^/(ca|kra|pki|acme)
+IPA
+systemctl reload httpd
