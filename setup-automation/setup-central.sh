db9b3aa9c27a42ea948916a0d3677bd8c0f1a71a
Nuno Martins <nmartins@redhat.com>
2026-03-15 14:08:20 +0200

    Update setup-central.sh

--- setup-automation/setup-central.sh
+++ setup-automation/setup-central.sh
@@ -17,11 +17,13 @@ subscription-manager register --org=${SATELLITE_ORG} --activationkey=${SATELLITE
 ## install python3 libraries needed for the Cloud Report
 dnf install -y python3-pip python3-libsemanage git ansible-core python-requests
 
+
 echo "192.168.1.10 control.zta.lab control" >> /etc/hosts
-echo "192.168.1.11  central.zta.lab  keycloak.zta.lab  opa.zta.lab" >> /etc/hosts
+echo "192.168.1.11 central.zta.lab  keycloak.zta.lab  opa.zta.lab" >> /etc/hosts
 echo "192.168.1.12 vault.zta.lab vault" >> /etc/hosts
 echo "192.168.1.13 wazuh.zta.lab wazuh" >> /etc/hosts
 echo "192.168.1.14 node01.zta.lab node01" >> /etc/hosts
+echo "192.168.1.15 netbox.zta.lab netbox" >> /etc/hosts
 
 nmcli connection add type ethernet con-name enp2s0 ifname enp2s0 ipv4.addresses 192.168.1.11/24 ipv4.method manual connection.autoconnect yes
 nmcli connection up enp2s0
