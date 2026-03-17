3b28ea79cf8c75f6f337784918d7a88f7829e81d
Nuno Martins <nmartins@redhat.com>
2026-03-17 12:51:41 +0200

    Update setup-control.sh

--- setup-automation/setup-control.sh
+++ setup-automation/setup-control.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 
-# systemctl stop systemd-tmpfiles-setup.service
-# systemctl disable systemd-tmpfiles-setup.service
+rm -rf /etc/yum.repos.d/*
+yum clean all
+subscription-manager clean
 
-# rm -rf /etc/yum.repos.d/*
-# yum clean all
-# subscription-manager clean
+retry() {
+    for i in {1..3}; do
+        echo "Attempt $i: $2"
+        if $1; then
+            return 0
+        fi
+        [ $i -lt 3 ] && sleep 5
+    done
+    echo "Failed after 3 attempts: $2"
+    exit 1
+}
 
-# retry() {
-#     for i in {1..3}; do
-#         echo "Attempt $i: $2"
-#         if $1; then
-#             return 0
-#         fi
-#         [ $i -lt 3 ] && sleep 5
-#     done
-#     echo "Failed after 3 attempts: $2"
-#     exit 1
-# }
+retry "curl -k -L https://${SATELLITE_URL}/pub/katello-server-ca.crt -o /etc/pki/ca-trust/source/anchors/${SATELLITE_URL}.ca.crt"
+retry "update-ca-trust"
+retry "rpm -Uhv --force https://${SATELLITE_URL}/pub/katello-ca-consumer-latest.noarch.rpm"
+retry "subscription-manager register --org=${SATELLITE_ORG} --activationkey=${SATELLITE_ACTIVATIONKEY} --force"
+retry "dnf install -y dnf-utils git nano"
+retry "dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo"
+retry "dnf install ipa-client sssd oddjob-mkhomedir-y"
+setenforce 0
 
-# retry "curl -k -L https://${SATELLITE_URL}/pub/katello-server-ca.crt -o /etc/pki/ca-trust/source/anchors/${SATELLITE_URL}.ca.crt"
-# retry "update-ca-trust"
-# retry "rpm -Uhv --force https://${SATELLITE_URL}/pub/katello-ca-consumer-latest.noarch.rpm"
-# retry "subscription-manager register --force --org=${SATELLITE_ORG} --activationkey=${SATELLITE_ACTIVATIONKEY} --force"
-# retry "dnf install -y dnf-utils git nano python3-pip python3-libsemanage git ipa-client sssd oddjob-mkhomedir"
-
-# setenforce 0
 
 
 echo "192.168.1.10 control.zta.lab control" >> /etc/hosts
