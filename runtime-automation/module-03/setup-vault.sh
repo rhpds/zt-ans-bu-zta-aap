83b65824d2f2db5c642d97f4382cd66c155d8b20
Nuno Martins <nmartins@redhat.com>
2026-04-15 12:27:27 +0200

    Update and rename setup-vault.sh to setup-central.sh

--- runtime-automation/module-03/setup-vault.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Unseal the Vault instance so users can immediately login at the UI.
-vault operator unseal -address=http://127.0.0.1:8200 -tls-skip-verify 1c6a637e70172e3c249f77b653fb64a820749864cad7f5aa7ab6d5aca5197ec5
-#
