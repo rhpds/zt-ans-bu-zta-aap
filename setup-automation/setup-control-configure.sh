a19bf4914f788ec5131a480a534572d3599d0c23
Nuno Martins <nmartins@redhat.com>
2026-04-16 12:12:43 +0200

    Update setup-control-configure.sh

--- setup-automation/setup-control-configure.sh
+++ setup-automation/setup-control-configure.sh
@@ -13,10 +13,9 @@ PLAYBOOK_DIR="/tmp/zta-workshop-aap"
 cd "${PLAYBOOK_DIR}" || { echo "ERROR: Cannot cd to ${PLAYBOOK_DIR}"; exit 1; }
 
 ansible-playbook -i inventory/hosts.ini setup/configure-aap-credentials.yml
-##ansible-playbook -i inventory/hosts.ini setup/configure-aap-ldap.yml
+ansible-playbook -i inventory/hosts.ini setup/deploy-dashboard.yml
 ansible-playbook -i inventory/hosts.ini setup/configure-aap-inventory.yml
 ansible-playbook -i inventory/hosts.ini setup/configure-aap-project.yml --tags ee,project
-#ansible-playbook -i inventory/hosts.ini setup/configure-aap-project.yml --tags ee,project,section1,rbac
 ansible-playbook -i inventory/hosts.ini setup/configure-aap-inventory.yml
 ansible-playbook -i inventory/hosts.ini setup/configure-aap-netbox.yml 
 ansible-playbook -i inventory/hosts.ini setup/configure-aap-project.yml --tags section1
