8ae00d47269b52ff43151bbfe6d558d42821c66e
Nuno Martins <nmartins@redhat.com>
2026-05-13 15:38:53 -0400

    Update setup-central.sh

--- runtime-automation/module-05/setup-central.sh
+++ runtime-automation/module-05/setup-central.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
- ansible-playbook -i /tmp/zta-workshop-aap/inventory/hosts.ini /tmp/zta-workshop-aap/setup/configure-aap-project.yml --tags remove_section -e aap_remove_section=4
- ansible-playbook -i /tmp/zta-workshop-aap/inventory/hosts.ini /tmp/zta-workshop-aap/setup/configure-aap-project.yml --tags section5,rbac
+# ansible-playbook -i /tmp/zta-workshop-aap/inventory/hosts.ini /tmp/zta-workshop-aap/setup/configure-aap-project.yml --tags remove_section -e aap_remove_section=4
+# ansible-playbook -i /tmp/zta-workshop-aap/inventory/hosts.ini /tmp/zta-workshop-aap/setup/configure-aap-project.yml --tags section5,rbac
 ## ansible-playbook -i /tmp/zta-workshop-aap/inventory/hosts.ini /tmp/zta-workshop-aap/setup/deploy-splunk.yml --skip-tags eda_webhook
