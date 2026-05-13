d98edf8ed61293f35d95354cffdf737e168e8725
Nuno Martins <nmartins@redhat.com>
2026-05-13 15:38:39 -0400

    Update setup-central.sh

--- runtime-automation/module-04/setup-central.sh
+++ runtime-automation/module-04/setup-central.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ansible-playbook -i /tmp/zta-workshop-aap/inventory/hosts.ini /tmp/zta-workshop-aap/setup/configure-aap-project.yml --tags remove_section -e aap_remove_section=3
-ansible-playbook -i /tmp/zta-workshop-aap/inventory/hosts.ini /tmp/zta-workshop-aap/setup/configure-aap-project.yml --tags section4,rbac
+#ansible-playbook -i /tmp/zta-workshop-aap/inventory/hosts.ini /tmp/zta-workshop-aap/setup/configure-aap-project.yml --tags remove_section -e aap_remove_section=3
+#ansible-playbook -i /tmp/zta-workshop-aap/inventory/hosts.ini /tmp/zta-workshop-aap/setup/configure-aap-project.yml --tags section4,rbac
