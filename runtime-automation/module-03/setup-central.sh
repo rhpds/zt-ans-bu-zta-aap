2cd80e3582ba1afb4bfdfba26075b355cc455763
Nuno Martins <nmartins@redhat.com>
2026-05-13 15:38:23 -0400

    Update setup-central.sh

--- runtime-automation/module-03/setup-central.sh
+++ runtime-automation/module-03/setup-central.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #ansible-playbook -i /tmp/zta-workshop-aap/inventory/hosts.ini /tmp/zta-workshop-aap/setup/configure-aap-project.yml --tags remove_section -e aap_remove_section=2
-ansible-playbook -i /tmp/zta-workshop-aap/inventory/hosts.ini /tmp/zta-workshop-aap/setup/configure-aap-project.yml --tags section3,rbac
+#ansible-playbook -i /tmp/zta-workshop-aap/inventory/hosts.ini /tmp/zta-workshop-aap/setup/configure-aap-project.yml --tags section3,rbac
 ipa group-remove-member team-infrastructure --users=neteng
 ipa group-add team-readonly --desc="Read-only template visibility (all workshop users)"
 ipa group-add-member team-readonly --users=neteng
