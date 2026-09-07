2ccb2207dc9e7be6b88a2aca4953baf34d9d51f6
Nuno Martins <nmartins@redhat.com>
2026-09-07 17:27:22 +0200

    Update setup-control.sh

--- setup-automation/setup-control.sh
+++ setup-automation/setup-control.sh
@@ -118,7 +118,7 @@ if [ -d /tmp/zta-workshop-aap/.git ]; then
 else
     rm -rf /tmp/zta-workshop-aap
     retry "Clone ZTA workshop repo" \
-        git clone -b main https://github.com/rhpds/lb2864-zta-aap-automation.git /tmp/zta-workshop-aap
+        git clone -b dev-sep https://github.com/rhpds/lb2864-zta-aap-automation.git /tmp/zta-workshop-aap
 fi
 
 # Ensure Ansible SSH ControlPath and fact-cache dirs are owned by the run user.
